1d4ef671544d6d2cc0525b5646c611c61fae80ec
Lukasz Kalemba <lukasz.kalemba@appstract.software>
2021-05-22 16:58:21 +0200

    Test on different devices

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -1,7 +1,26 @@
 /// <reference types="cypress" />
 
-describe('Empty test', () => {
-  it('test one', () => {
+describe('Basic Tests', () => {
+  it('Every basic element exists', () => {
     cy.visit('http://codedamn.com/');
+
+    cy.contains('Learn web development');
+
+    cy.contains('Get Pro - Unlock everything.').should(
+      'have.text',
+      'Get Pro - Unlock everything.'
+    );
+  });
+
+  it('Every basic element exists on mobile', () => {
+    cy.viewport('iphone-6');
+    cy.visit('http://codedamn.com/');
+
+    cy.contains('Learn web development');
+
+    cy.contains('Get Pro - Unlock everything.').should(
+      'have.text',
+      'Get Pro - Unlock everything.'
+    );
   });
 });
