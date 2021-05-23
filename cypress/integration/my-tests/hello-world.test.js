e6a116a682d3541ffaa3cf65de2a2dcef1a1a24f
Lukasz Kalemba <lukasz.kalemba@appstract.software>
2021-05-23 09:16:38 +0200

    Check if login page works fine

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -44,4 +44,43 @@ describe('Basic Tests', () => {
     cy.url().should('include', '/learn');
     cy.go('back');
   });
+
+  it('Login should display correct error', () => {
+    cy.visit('http://codedamn.com/');
+    cy.viewport('macbook-16');
+
+    cy.get('[href="/settings"]').click();
+
+    cy.url().should('include', '/login');
+
+    cy.get('[data-testid=snackclose]').should('not.exist');
+
+    cy.get('[data-testid=username]').type('lukasz.kalemba1@gmail.com');
+    cy.get('[data-testid=password]').type('zaq1@WSX');
+    cy.get('[data-testid=login]').click();
+
+    cy.get('[data-testid=snackclose]').should('exist');
+  });
+
+  // run only this one
+  it.only('Login should work fine', () => {
+    // TODO: Set this as localStorage token for authentication
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imlvc2RldiIsIl9pZCI6IjViNzM2N2JiYzdjNDU1MTA5ZTFmNzI4YyIsIm5hbWUiOiJpb3NkZXYiLCJpYXQiOjE2MjE3NTQ4NDIsImV4cCI6MTYyNDM0Njg0Mn0.UXVx8uTGF2c-NSZFdQbHsUJBUmpiM5VrgzDKx4Ch5r4';
+
+    cy.visit('http://codedamn.com/');
+    cy.viewport('macbook-16');
+
+    cy.get('[href="/settings"]').click();
+
+    cy.url().should('include', '/login');
+
+    cy.get('[data-testid=username]').type('iosdev');
+    cy.get('[data-testid=password]').type('iosdev');
+    cy.get('[data-testid=login]').click();
+
+    cy.get('[data-testid=snackclose]').should('not.exist');
+
+    cy.url().should('include', '/dashboard');
+  });
 });
